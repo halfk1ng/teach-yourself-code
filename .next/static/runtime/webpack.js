a4402bfe4f4e8fd7620c8ed9d12b75f3d5ec46d0
James C. Tucker <jamesctucker@protonmail.com>
2020-04-10 09:22:22 -0500

    decided to go with redux toolkit for state instead of context; removed unused dependencies

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f3274be823868590041d";
+/******/ 	var hotCurrentHash = "15ee0095d52aaf553d30";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
