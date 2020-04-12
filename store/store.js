a1d4af2670db366c4718900f54c1dd53ed68c359
James C. Tucker <jamesctucker@protonmail.com>
2020-04-12 09:00:20 -0500

    Added initial state for currentVideo and added reducer

--- store/store.js
+++ store/store.js
@@ -1,11 +1,13 @@
 import { configureStore, createReducer } from "@reduxjs/toolkit";
 
 const initialState = {
-  currentUser: "James"
+  currentUser: "James",
+  currentVideo: undefined
 };
 
 const reducer = createReducer(initialState, {
-  UPDATE_CURRENT_USER: (state, action) => ({ currentUser: action.user })
+  UPDATE_CURRENT_USER: (state, action) => ({ currentUser: action.user }),
+  UPDATE_CURRENT_VIDEO: (state, action) => ({ currentVideo: action.video })
 });
 
 const initializeStore = (preloadedState = initialState) => {
