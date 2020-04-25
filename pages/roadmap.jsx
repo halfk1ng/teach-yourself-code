83f1d2b933785cfa6aa5fa5fba84a22c04ed5879
James C. Tucker <jamesctucker@protonmail.com>
2020-04-25 01:41:25 -0500

    added content and styles to roadmap

--- pages/roadmap.jsx
+++ pages/roadmap.jsx
@@ -3,7 +3,28 @@ import Layout from "../components/Layout/index";
 function Roadmap() {
   return (
     <Layout>
-      <h3 className="title">Roadmap</h3>
+      <div className="roadmap-container">
+        <h3 className="title">Roadmap</h3>
+        <p className="subtitle">
+          A list of update releases and planned improvements
+        </p>
+        <div className="roadmap-section">
+          <h2 className="title is-size-5 roadmap-header">Recent Updates</h2>
+          <ul>
+            <li>none yet</li>
+          </ul>
+        </div>
+
+        <div className="roadmap-section">
+          <h2 className="title is-size-5 roadmap-header">Planned Features</h2>
+          <ul>
+            <li>Save user progress for each tutorial</li>
+            <li>
+              <s>User can add note for specific video</s>
+            </li>
+          </ul>
+        </div>
+      </div>
     </Layout>
   );
 }
