7d925f590f5ae0528d10057eef58573f8fd0fb51
James C. Tucker <jamesctucker@protonmail.com>
2020-05-02 00:17:15 -0500

    updated roadmap with content

--- pages/roadmap.jsx
+++ pages/roadmap.jsx
@@ -8,21 +8,34 @@ function Roadmap() {
         <p className="subtitle">
           A list of update releases and planned improvements
         </p>
-        <div className="roadmap-section">
-          <h2 className="title is-size-5 roadmap-header">Recent Updates</h2>
-          <ul>
-            <li>none yet</li>
-          </ul>
-        </div>
-
-        <div className="roadmap-section">
-          <h2 className="title is-size-5 roadmap-header">Planned Features</h2>
-          <ul>
-            <li>Save user progress for each tutorial</li>
-            <li>
-              <s>User can add note for specific video</s>
-            </li>
-          </ul>
+        <div className="columns">
+          <div className="column">
+            <div className="roadmap-section">
+              <h2 className="title is-size-5 roadmap-header">Recent Updates</h2>
+              <ul>
+                <li>n/a</li>
+              </ul>
+            </div>
+          </div>
+          <div className="column is-centered is-vcentered">
+            <div className="roadmap-section">
+              <h2 className="title is-size-5 roadmap-header">
+                Planned Features
+              </h2>
+              <ul>
+                <li>- Save user progress for each tutorial</li>
+                <li>- Add most recently-watched video to dashboard</li>
+                <li>- Show tutorial progress indicator</li>
+                <li>- Filter subscriptions by topic</li>
+                <li>- Users can edit and delete notes</li>
+                <li>- Users can add custom tutorials</li>
+                <li>- Add single videos for topics</li>
+                <li>
+                  <s>- User can add note to specific video</s>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
