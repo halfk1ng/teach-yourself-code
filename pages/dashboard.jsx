771bf5ba283396ad101af5effc453440adf5b471
James C. Tucker <jamesctucker@protonmail.com>
2020-05-02 00:16:50 -0500

    centered dashboard view

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -7,14 +7,18 @@ function Dashboard() {
   const user = useSelector((state) => state.user.currentUser);
   return (
     <Layout className="is-flex">
-      <div>
-        {!user ? <Loader /> : <h3 class="title">👋 Hi, {user.nickname}!</h3>}
-        <Link href={"/subscriptions"}>
-          <a className="signup-btn" style={{ margin: "0 auto" }}>
-            Go to your subscriptions!
-          </a>
-        </Link>
-      </div>
+      {!user ? (
+        <Loader />
+      ) : (
+        <div className="dashboard-view">
+          <h3 class="title">👋 Hi, {user.nickname}!</h3>
+          <Link href={"/subscriptions"}>
+            <a className="signup-btn" style={{ margin: "0 auto" }}>
+              Go to your subscriptions!
+            </a>
+          </Link>
+        </div>
+      )}
     </Layout>
   );
 }
