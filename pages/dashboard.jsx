d47e2f704b938f87139d5469e0f267be4515aea4
James C. Tucker <jamesctucker@protonmail.com>
2020-05-01 20:11:30 -0500

    added subscriptions btn to dashboard

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,13 +1,19 @@
 import Layout from "../components/Layout";
 import { useSelector } from "react-redux";
 import Loader from "../components/Loader";
+import Link from "next/link";
 
 function Dashboard() {
   const user = useSelector((state) => state.user.currentUser);
   return (
     <Layout className="is-flex">
       <div>
-        {!user ? <Loader /> : <h3 class="title">Hi, {user.nickname}</h3>}
+        {!user ? <Loader /> : <h3 class="title">👋 Hi, {user.nickname}!</h3>}
+        <Link href={"/subscriptions"}>
+          <a className="signup-btn" style={{ margin: "0 auto" }}>
+            Go to your subscriptions!
+          </a>
+        </Link>
       </div>
     </Layout>
   );
